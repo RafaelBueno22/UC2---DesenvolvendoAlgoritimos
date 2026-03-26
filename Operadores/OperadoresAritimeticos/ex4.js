5f3cdeb0c397db783f37293572e8b5ecd67b597e
RAFAEL CORREA BUENO <04504323001@senacrs.edu.br>
2026-03-26 17:25:33 -0300

    trabalhando com operadores

---
+++ Operadores/OperadoresAritimeticos/ex4.js
@@ -0,0 +1,32 @@
+/*const segunda = 4
+const terça = 6
+
+const quarta = segunda * 2 + terça + segunda
+
+console.log(quarta) */
+
+/*const tinha = 50
+const ganhou = 30
+const gastou = 20
+const resultado = tinha + ganhou - gastou
+
+console.log(resultado) */
+
+/*const pizzas = 40
+const refrigerantes = 12
+const total = pizzas * 3 + refrigerantes *2 
+const desconto = total * 0.10
+const valor = total - desconto 
+const valorDividido3 = valor / 3
+
+console.log(valorDividido3.toFixed(2))*/
+
+
+const moedaInicial = 150
+const missao = 40
+const item = 95
+const total = moedaInicial + (missao * 3) - item
+const totalcomDesconto = total / 5
+
+
+console.log(totalcomDesconto)
