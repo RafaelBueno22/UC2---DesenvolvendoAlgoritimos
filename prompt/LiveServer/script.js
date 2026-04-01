6161162a336982daf6f87a7920f2374a053b798f
RAFAEL CORREA BUENO <04504323001@senacrs.edu.br>
2026-04-01 17:13:01 -0300

    prompt

---
+++ prompt/LiveServer/script.js
@@ -0,0 +1,6 @@
+//pede o nome do usuario usando uma caixinha de prompt
+//oq o usuario digitar será salvo na variavel"nome"
+let nome = prompt("Qual o seu nome?")
+let mensagem = "Olá " +  nome + "! Seja bem-vindo(a)"
+//mostra a mensagem na tela usando uma caixinha de alerta
+alert(mensagem)
---
+++ prompt/index.js
@@ -0,0 +1,27 @@
+/*const prompt = require('prompt-sync')()
+
+let nome = prompt ("Qual o seu nome?")
+let idade = prompt ("Qual a sua idade")
+
+console.log(`Olá ${nome} você tem ${idade} anos de idade.`)*/
+
+
+
+
+const prompt = require ('prompt-sync')()
+
+/*let nome = prompt ("Qual o nome do seu personagem?")
+let profissao = prompt ("Qual a sua profissão?")
+let objetoQueUsa = prompt ("Qual objeto ou arma você usa no seu trabalho?")
+
+console.log(`O meu personagem é o ${nome}, sua profissão é ${profissao} e para salvar a princesa ele usa uma ${objetoQueUsa}.`)*/
+
+let nome = prompt("Qual o seu nome?")
+let idade = prompt ("Qual a sua idade?")
+let gostaDeFazer = prompt ("O que você gosta de fazer?")
+let anime = prompt ("Qual o seu anime favorito?")
+let serie = prompt ("Qual a sua série favorita?")
+let curiosdade = prompt ("Me conte uma curiosidade sua:")
+let time = prompt ("Qual o seu time de coração?")
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos, eu gosto de ${gostaDeFazer} no meu tempo livre, meu anime favorito é ${anime} e a minha série favorita é ${serie}. Uma curiosidade minha é que eu ${curiosdade} e o meu time de coração é o ${time}.`)
